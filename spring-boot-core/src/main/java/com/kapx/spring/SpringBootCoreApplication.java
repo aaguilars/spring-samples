f75c8c304f788433d711a22d564057a24dc174c8
KAPIL <xkapil@gmail.com>
2016-04-11 15:31:58 +0100

    sprint boot initial version

---
+++ spring-boot-core/src/main/java/com/kapx/spring/SpringBootCoreApplication.java
@@ -0,0 +1,12 @@
+package com.kapx.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCoreApplication.class, args);
+	}
+}
